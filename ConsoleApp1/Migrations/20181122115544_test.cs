e312c71728ade76e4e6efa79fa6c659be7746525
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2018-11-22 11:56:33 +0000

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Migrations/20181122115544_test.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp1.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EditorOptionsBase",
+                columns: table => new
+                {
+                    EditorType = table.Column<int>(nullable: false),
+                    ReadOnly = table.Column<bool>(nullable: false),
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Mask = table.Column<string>(nullable: true),
+                    MaskChar = table.Column<string>(nullable: true),
+                    MaskInvalidMessage = table.Column<string>(nullable: true),
+                    Mode = table.Column<int>(nullable: true),
+                    Placeholder = table.Column<string>(nullable: true),
+                    ShowClearButton = table.Column<bool>(nullable: true),
+                    ShowMaskMode = table.Column<int>(nullable: true),
+                    Spellcheck = table.Column<bool>(nullable: true),
+                    Format = table.Column<string>(nullable: true),
+                    InvalidValueMessage = table.Column<string>(nullable: true),
+                    Min = table.Column<double>(nullable: true),
+                    Max = table.Column<double>(nullable: true),
+                    ShowSpinButtons = table.Column<bool>(nullable: true),
+                    Step = table.Column<double>(nullable: true),
+                    UseLargeSpinButtons = table.Column<bool>(nullable: true),
+                    EditorOptionsTextArea_Mask = table.Column<string>(nullable: true),
+                    EditorOptionsTextArea_MaskChar = table.Column<string>(nullable: true),
+                    EditorOptionsTextArea_MaskInvalidMessage = table.Column<string>(nullable: true),
+                    EditorOptionsTextArea_Mode = table.Column<int>(nullable: true),
+                    EditorOptionsTextArea_Placeholder = table.Column<string>(nullable: true),
+                    EditorOptionsTextArea_ShowClearButton = table.Column<bool>(nullable: true),
+                    EditorOptionsTextArea_ShowMaskMode = table.Column<int>(nullable: true),
+                    EditorOptionsTextArea_Spellcheck = table.Column<bool>(nullable: true),
+                    MaxLength = table.Column<int>(nullable: true),
+                    AutoResizeEnabled = table.Column<bool>(nullable: true),
+                    MaxHeight = table.Column<double>(nullable: true),
+                    MinHeight = table.Column<double>(nullable: true),
+                    EditorOptionsTextBox_Mask = table.Column<string>(nullable: true),
+                    EditorOptionsTextBox_MaskChar = table.Column<string>(nullable: true),
+                    EditorOptionsTextBox_MaskInvalidMessage = table.Column<string>(nullable: true),
+                    EditorOptionsTextBox_Mode = table.Column<int>(nullable: true),
+                    EditorOptionsTextBox_Placeholder = table.Column<string>(nullable: true),
+                    EditorOptionsTextBox_ShowClearButton = table.Column<bool>(nullable: true),
+                    EditorOptionsTextBox_ShowMaskMode = table.Column<int>(nullable: true),
+                    EditorOptionsTextBox_Spellcheck = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EditorOptionsBase", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EditorOptionsBase");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ConsoleApp1
+{
+    public enum EditorType
+    {
+        dxAutocomplete = 0,
+        dxCalendar = 1,
+        dxCheckBox = 2,
+        dxColorBox = 3,
+        dxDateBox = 4,
+        dxDropDownBox = 5,
+        dxLookup = 6,
+        dxNumberBox = 7,
+        dxRadioGroup = 8,
+        dxRangeSlider = 9,
+        dxSelectBox = 10,
+        dxSlider = 11,
+        dxSwitch = 12,
+        dxTagBox = 13,
+        dxTextArea = 14,
+        dxTextBox = 15
+    }
+
+    public abstract class EditorOptionsBase
+    {
+        public EditorType EditorType { get; private set; }
+        public bool ReadOnly { get; set; } = false;
+    }
+
+    public enum MaskMode
+    {
+        always,
+        onFocus
+    }
+
+    public enum TextBoxMode
+    {
+        email,
+        password,
+        search,
+        tel,
+        text,
+        url
+    }
+
+    public abstract class EditorOptionsText : EditorOptionsBase
+    {
+        public string Mask { get; set; }
+        public string MaskChar { get; set; }
+        public string MaskInvalidMessage { get; set; }
+        public TextBoxMode? Mode { get; set; }
+        public string Placeholder { get; set; }
+        public bool ShowClearButton { get; set; }
+        public MaskMode ShowMaskMode { get; set; }
+        public bool Spellcheck { get; set; }
+    }
+
+    public class EditorOptionsTextBox : EditorOptionsText
+    {
+    }
+
+    public class EditorOptionsTextArea : EditorOptionsText
+    {
+        public int? MaxLength { get; set; }
+
+        public bool AutoResizeEnabled { get; set; }
+        public double? MaxHeight { get; set; }
+        public double? MinHeight { get; set; }
+    }
+
+    public class EditorOptionsNumberBox : EditorOptionsText
+    {
+        public string Format { get; set; }
+        public string InvalidValueMessage { get; set; }
+        public double? Min { get; set; }
+        public double? Max { get; set; }
+        public bool ShowSpinButtons { get; set; }
+        public double Step { get; set; }
+        public bool UseLargeSpinButtons { get; set; }
+    }
+
+    public class EditorOptionsCalendar : EditorOptionsBase
+    {
+    }
+    class MyContext : DbContext
+    {
+        public MyContext() : base(new DbContextOptionsBuilder()
+                .UseSqlServer(@"Data Source = (LocalDb)\MSSQLLocalDB; Initial Catalog = Test;").Options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var editorOptionsBase = modelBuilder.Entity<EditorOptionsBase>();
+            editorOptionsBase.Property(typeof(int), "Id");
+            editorOptionsBase.HasKey("Id");
+            editorOptionsBase.HasDiscriminator<EditorType>(nameof(EditorType)).
+                HasValue<EditorOptionsNumberBox>(EditorType.dxNumberBox).
+                HasValue<EditorOptionsTextBox>(EditorType.dxTextBox).
+                HasValue<EditorOptionsTextArea>(EditorType.dxTextArea).
+                HasValue<EditorOptionsCalendar>(EditorType.dxCalendar);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
